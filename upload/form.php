af36d8d8a3fb18b7313f5f2248fe700646914ce2
Hoang Viet Anh <vietanh092004@gmail.com>
2024-01-13 17:57:59 +0700

    PHP

---
+++ upload/form.php
@@ -0,0 +1,5 @@
+<form method="post" action="tackle.php" enctype="multipart/form-data">
+    <input type="file">
+    <button type="submit">upload</button>
+    date_default_timezone_get()
+</form>
---
+++ upload/tackle.php
@@ -0,0 +1,8 @@
+<?php
+    if(!empty($_SERVER['REQUEST_METHOD'])){
+        echo '<pre>';
+        print_r($_FILES);
+        echo '</pre>';
+
+    }
+?>
